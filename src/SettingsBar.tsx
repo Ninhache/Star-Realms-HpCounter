e26e3dbaf8bf3857624246ec274ab263e045ab4a
Ninhache <mr.carambar27@gmail.com>
2024-03-07 16:18:48 +0100

    feat: started a feature to choose a random player from the actual players

--- src/SettingsBar.tsx
+++ src/SettingsBar.tsx
@@ -5,17 +5,33 @@ import {
     TouchableOpacity,
     View
 } from 'react-native';
+import { Layout } from './App';
 import SettingsModal from './SettingsModal';
 
 const settingsImage = require("../assets/images/settings.png")
 
-const SettingsBar: React.FC<{}> = ({ }) => {
+interface SettingsBarProps {
+    layout?: Layout,
+    onPlayerSelect?: (playerId: number) => void;
+    playersLength: number;
+  }
+
+const SettingsBar: React.FC<SettingsBarProps> = ({ playersLength, onPlayerSelect = () => {console.log("UNDEFINED FUNCTION")} , layout = "HORIZONTAL" }) => {
 
     const [isModalVisible, setModalVisible] = useState(false);
+
+    const selectRandomly = (): void => {
+        console.log("choose player X from the list []");
+        onPlayerSelect(Math.round(Math.random() * (playersLength - 1)));
+    }
 
     return (
         <View style={styles.container}>
             <TouchableOpacity onPress={() => setModalVisible(true)}>
+                <Image style={styles.settingsImage} source={settingsImage} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => selectRandomly()}>
                 <Image style={styles.settingsImage} source={settingsImage} />
             </TouchableOpacity>
 
@@ -26,6 +42,9 @@ const SettingsBar: React.FC<{}> = ({ }) => {
 
 const styles = StyleSheet.create({
     container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         backgroundColor: "rgb(34,34,34)"
     },
     settingsImage: {
